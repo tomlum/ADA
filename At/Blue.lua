11f3cdea9d5d988888f93c83a044fb836173b985
thlum <checkeredlines@gmail.com>
2014-11-25 21:50:18 -0500

    midwaythrough

--- At/Blue.lua
+++ At/Blue.lua
@@ -10,41 +10,45 @@ yd = 0
 you.bluechargetime = 50
 you.airready = true
 youairready = true
+you.d = 0
+you.next = 0
+you.walktimer
 
---ybancel to y.bcancel, youblu to yy.blu, yd - y.d
+
+--ybancel to yy.bcancel, youblu to yy.blu, yd - y.d
 --younext yy.next
+--walktimery - yy.walktimer
+--ybp to yy.bp 
 --blues2 to yy.blues
---[[
+--you and me.id which is the number 1 or 2
+--youairready to you.airready
+--youbluepause yy.bluepause
+		--youblu yy.blu 
+    --youbjstop to yy.bjstop
+--walktimery - yy.walktimer
+
+
 function blueblade(x, yy, mm, cy, cm)
   
-
 	if yy.g then yy.airready = true
 end
 
-
 	local xdif = (mm.mid - yy.mid) * yy.lr
-
 
 	if yy.g then yy.airready = true
 	end
 
-
-	if yy.bluepause <= 0 or yy.flinch or y.bcancel then 
+	if yy.bluepause <= 0 or yy.flinch or yy.bcancel then 
 		yy.next = 0
 		yy.bluechargetimer = 0
 		yy.blu = false
 		yy.d = 0
-
 	else yy.blu = true
 	end
-
-	if not yy..g then
-
+	if not yy.g then
 	
-
 	if yy.next == "nevergonnahappen"
 		then yy.next = "oh it happened"
-
 
 	elseif yy.next == 202 and not yy.g
 		then 
@@ -60,11 +64,10 @@ end
   hboxc({x = yy.mid, y= yy.y+30}, 
     {x = yy.mid, y= yy.y-200-yy.j},
     {x = yy.mid+200*yy.lr+yy.v, y= yy.y-200-yy.j},
-    {x = yy.mid+yy.v, y= yy.y+30}, true, yy.lr, true, true, yy.v + (5*yy.lr), yy.j+6, airbluedam, true, abft, 0, 0, false, false, 2)
+    {x = yy.mid+yy.v, y= yy.y+30}, true, yy.lr, true, true, yy.v + (5*yy.lr), yy.j+6, airbluedam, true, abft, 0, 0, false, false, yy.id)
 			
       
 			--me.g = false ?!?!?!?!?!?!?!?!?!!?!?
-
 
 		y.d = 8
 		yy.im = brise 
@@ -77,261 +80,211 @@ end
 		yy.v = yy.v + .2 * yy.lr
 		repplay(yy.blues)
 
-
-
-		elseif (yy.j < 0 and not cy.up) or (controller2.down and you.j >= 0) then
-		yd = 6
-
+		elseif (yy.j < 0 and not cy.up) or (cy.down and yy.j >= 0) then
+		yy.d = 6
 		if (cy.down and yy.j >=0) then yy.j = -30
 		else
 		yy.j = yy.j - 5
-
-		repplay(blues2)
+		repplay(yy.yy.blues)
 		end
-		you.v = you.v + 1 * you.lr
-		if youarmsreach(200) and me.y > you.y and me.y < you.y + 200 and ybp >=12 and not me.dodge
-			then 
-			me.v = you.v + (10 * you.lr)
-			if not (me.block and me.lr ~= you.lr) then
-			me.flinch = true
-			me.health = me.health - airbluedam
-			me.ft = abft
-			me.j = you.j - 20
-			end
-		end
+		yy.v = yy.v + 1 * yy.lr
+    hboxc({x = yy.mid, y= yy.y+30}, 
+    {x = yy.mid, y= yy.y+200-yy.j},
+    {x = yy.mid+200*yy.lr+yy.v, y= yy.y+200-yy.j},
+    {x = yy.mid+yy.v, y= yy.y+30}, true, yy.lr, true, true, yy.v + (10*yy.lr), yy.j-20, airbluedam, true, abft, 0, 0, false, false, yy.id)
+  
+		
 
-
-
-		if ybp == 24 then you.im = bfall
-		elseif ybp == 18
-		then you.im = bfall2
-		else you.im = bfall3
+		if yy.bp == 24 then yy.im = bfall
+		elseif yy.bp == 18
+		then yy.im = bfall2
+		else yy.im = bfall3
 		end
 		end
-
-	elseif youairready and x and not you.g and younext <=201 then
-		if you.j >= 0 then 
-			you.im = briseprep
-		else you.im = bfallprep
+	elseif yy.airready and x and not yy.g and yynext <=201 then
+		if yy.j >= 0 then 
+			yy.im = briseprep
+		else yy.im = bfallprep
 		end
-		younext = 200
-		yd = 24
-		you.bluepause = 30
-		youblu = false
-
-	else yd = 0
-		you.bluechargetimer = 0
-		youbjstop = false
+		yy.next = 200
+		yy.d = 24
+		yy.bluepause = 30
+		yy.blu = false
+	else yy.d = 0
+		yy.bluechargetimer = 0
+		yy.bjstop = false
 	end
-
 
 else
-
-	if younext==20 then you.im = bc2 
-			yd = 24
-      hc(you.mid, you.y+30, you.mid+you.v+170*you.lr, you.y+30-you.j, true, you.lr, true, true, you.v + (80*you.lr), you.j, bluechargedam, true, bcft, 0, 0, false, false, 2)
-		if youarmsreach(170) and sameheight() and not me.invince then 
-				me.flinch = true
-				me.ft = bcft
-				youactionshot = true
-				me.v = 80 * you.lr
-				me.health = me.health - bluechargedam
-		end
+	if yy.next==20 then yy.im = bc2 
+			yy.d = 24
+      hc(yy.mid, yy.y+30, yy.mid+yy.v+170*yy.lr, yy.y+30-yy.j, true, yy.lr, true, true, yy.v + (80*yy.lr), yy.j, bluechargedam, true, bcft, 0, 0, false, false, 2)
 			
-	elseif younext==21 then you.im = bc2 
-			yd = 24
+	elseif yynext==21 then yy.im = bc2 
+			yy.d = 24
 			repplay(bcs2)
-	elseif younext==22 then you.im = bc3 
-			yd = 12
-			if ybp > 20 and platformcheck(you.mid + (50*you.lr),you.y,-70) and platformcheck(you.mid + (100*you.lr),you.y,-70) then
-				makefloorrubble(you.mid + (50*you.lr), you.feet, 3*you.lr,4)
+	elseif yynext==22 then yy.im = bc3 
+			yy.d = 12
+			if yy.bp > 20 and platformcheck(yy.mid + (50*yy.lr),yy.y,-70) and platformcheck(yy.mid + (100*yy.lr),yy.y,-70) then
+				makefloorrubble(yy.mid + (50*yy.lr), you.feet, 3*yy.lr,4)
 			end
-	elseif younext>=23 and younext < 100 then you.im = bcend
-			yd = 0 
+	elseif yy.next>=23 and yy.next < 100 then yy.im = bcend
+			yy.d = 0 
 
-
-	elseif not x and younext == 19 
-		then you.im = bc1
-		yd = 12
+	elseif not x and yy.next == 19 
+		then yy.im = bc1
+		yy.d = 12
 		
 
+	elseif yy.bluechargetimer > bluechargetime and x and yy.next <= 19
+		then yy.im = bluecharging
+		yy.bluepause = 30
+		yy.blu = false
+		yy.d = 24
+		yy.bp = 24
+		yy.next = 18
 
-	elseif you.bluechargetimer > bluechargetime and x and younext <= 19
-		then you.im = bluecharging
-		you.bluepause = 30
-		youblu = false
-		yd = 24
-		ybp = 24
-		younext = 18
-
+elseif yy.next == 12 then
+	yy.im = bcend
+	yy.d = 0
+elseif yy.next == 11 then
+	yy.im = bcend
+	yy.d = 12
+	yy.v = yy.v + (-8 * yy.lr)
+elseif yy.next == 10 and not x
+		then yy.im = blue33
+		yy.d = 12
+		yy.bluechargetimer = 0
+   hc(you.mid, you.y+30, you.mid+yy.v+250*yy.lr, you.y+30-yy.j, true, yy.lr, true, true, yy.v + (20*yy.lr), yy.j, bluedam, true, bft, 0, 0, false, false, 2)
+elseif yy.next == 9 and not x
+		then yy.im = blue32
+		yy.d = 24
+		yy.bluechargetimer = 0
 	
-
-
-
-elseif younext == 12 then
-	you.im = bcend
-	yd = 0
-elseif younext == 11 then
-	you.im = bcend
-	yd = 12
-	you.v = you.v + (-8 * you.lr)
-
-elseif younext == 10 and not x
-		then you.im = blue33
-		yd = 12
-		you.bluechargetimer = 0
-   hc(you.mid, you.y+30, you.mid+you.v+250*you.lr, you.y+30-you.j, true, you.lr, true, true, you.v + (20*you.lr), you.j, bluedam, true, bft, 0, 0, false, false, 2)
-
-elseif younext == 9 and not x
-		then you.im = blue32
-		yd = 24
-		you.bluechargetimer = 0
-	
-	repplay(blues2)
-elseif younext == 8 and not x
-		then you.im = blue32
-		yd = 24
-		you.bluechargetimer = 0
-elseif younext == 7 and not x
-		then you.im = blue31
-		yd = 12
-		you.bluechargetimer = 0
-
+	repplay(yy.blues)
+elseif yy.next == 8 and not x
+		then yy.im = blue32
+		yy.d = 24
+		yy.bluechargetimer = 0
+elseif yy.next == 7 and not x
+		then yy.im = blue31
+		yy.d = 12
+		yy.bluechargetimer = 0
 		
-elseif x and younext == 6 or younext == 7 then 
-		you.im = blue3start
-		younext = 7
-		you.bluepause = 25
-		youblu = false
-		you.bluechargetimer = you.bluechargetimer + 1
+elseif x and yy.next == 6 or yy.next == 7 then 
+		yy.im = blue3start
+		yy.next = 7
+		yy.bluepause = 25
+		yy.blu = false
+		yy.bluechargetimer = yy.bluechargetimer + 1
 
 
 
-
-
-
-
-	elseif younext == 6
-		then you.im = blue23
-		yd = 0
-	elseif younext == 5
-		then you.im = blue22
-		yd = 8
-		repplay(blues2)	
-    hc(you.mid, you.y+30, you.mid+you.v+160*you.lr, you.y+30-you.j, true, you.lr, true, true, you.v + (4*you.lr), you.j, bluedam, true, bft/2, 0, 0, false, false, 2)
-		--hc(you.mid, you.y+30, you.mid+you.v+160*you.lr, you.y+30-you.j, true, you.lr, true, true, you.v + (4*you.lr), you.j, bluedam, true, bft, 0, 0, false, false, 2)
-
-	elseif younext == 4 and not x
-		then you.im = blue21
-		if controller2.right and you.lr > 0 then
-			you.v = you.v + 8
+	elseif yy.next == 6
+		then yy.im = blue23
+		yy.d = 0
+	elseif yy.next == 5
+		then yy.im = blue22
+		yy.d = 8
+		repplay(yy.blues)	
+    hc(you.mid, you.y+30, you.mid+yy.v+160*yy.lr, you.y+30-yy.j, true, yy.lr, true, true, yy.v + (4*yy.lr), yy.j, bluedam, true, bft/2, 0, 0, false, false, 2)
+		--hc(you.mid, you.y+30, you.mid+yy.v+160*yy.lr, you.y+30-yy.j, true, yy.lr, true, true, yy.v + (4*yy.lr), yy.j, bluedam, true, bft, 0, 0, false, false, 2)
+	elseif yy.next == 4 and not x
+		then yy.im = blue21
+		if controller2.right and yy.lr > 0 then
+			yy.v = yy.v + 8
 		elseif
-			controller2.left and you.lr < 0 then 
-			you.v = you.v - 8
+			controller2.left and yy.lr < 0 then 
+			yy.v = yy.v - 8
 		else
-		you.v = you.v + (4 * you.lr)
+		yy.v = yy.v + (4 * yy.lr)
 		end
-		yd = 12
-		you.bluechargetimer = 0
-	elseif x and younext == 3 or younext == 4 then 
-		you.im = blue2start
-		younext = 4
-		you.bluepause = 25
-		youblu = false
-		you.bluechargetimer = you.bluechargetimer + 1
+		yy.d = 12
+		yy.bluechargetimer = 0
+	elseif x and yy.next == 3 or yy.next == 4 then 
+		yy.im = blue2start
+		yy.next = 4
+		yy.bluepause = 25
+		yy.blu = false
+		yy.bluechargetimer = yy.bluechargetimer + 1
 
 
-
-
-	elseif younext == 3 then
-		you.im = blue1end
-		you.stop = true
-		yd = 0
-	elseif younext == 2
+	elseif yy.next == 3 then
+		yy.im = blue1end
+		yy.stop = true
+		yy.d = 0
+	elseif yy.next == 2
 		then
-		you.im = blue12
-		yd = 12
-		repplay(blues2)		
+		yy.im = blue12
+		yy.d = 12
+		repplay(yy.blues)		
    
-		hc(you.mid, you.y+30, you.mid+you.v+160*you.lr, you.y+30-you.j, true, you.lr, true, true, you.v + (4*you.lr), you.j, bluedam, true, bft/2, 0, 0, false, false, 2)
+		hc(you.mid, you.y+30, you.mid+yy.v+160*yy.lr, you.y+30-yy.j, true, yy.lr, true, true, yy.v + (4*yy.lr), yy.j, bluedam, true, bft/2, 0, 0, false, false, 2)
     
-    --hcCheck(you.mid, you.y+30, you.mid+you.v, you.y+30-you.j, 2)
-	elseif younext == 1 and not x then
-		you.im = blue11
-		yd = 24
-		if controller2.right and you.lr > 0 then
-			you.v = you.v + 7
+    --hcCheck(you.mid, you.y+30, you.mid+yy.v, you.y+30-yy.j, 2)
+	elseif yy.next == 1 and not x then
+		yy.im = blue11
+		yy.d = 24
+		if controller2.right and yy.lr > 0 then
+			yy.v = yy.v + 7
 		elseif
-			controller2.left and you.lr < 0 then 
-			you.v = you.v - 7
+			controller2.left and yy.lr < 0 then 
+			yy.v = yy.v - 7
 		else
-		you.v = you.v + (6 * you.lr)
-
+		yy.v = yy.v + (6 * yy.lr)
 		end
-		you.bluechargetimer = 0
-	elseif x and younext == 0 or younext == 1 then 
-		you.im = blue1start
-		younext = 1
-		you.bluepause = 23
-		youblu = false
-		you.bluechargetimer = you.bluechargetimer + 1
-		yd = 0
-
-		if walktimery< 7 then 
-		you.im = bwalk1
-	elseif walktimery>= 7 and walktimery< 14 then
-		you.im = bwalk2
-	elseif walktimery>= 14 and walktimery< 21 then
-		you.im = bwalk3
-	elseif walktimery>= 21 and walktimery< 28 then
-		you.im = bwalk4
-	elseif walktimery>= 28 and walktimery< 35 then
-		you.im = bwalk5
+		yy.bluechargetimer = 0
+	elseif x and yy.next == 0 or yy.next == 1 then 
+		yy.im = blue1start
+		yy.next = 1
+		yy.bluepause = 23
+		yy.blu = false
+		yy.bluechargetimer = yy.bluechargetimer + 1
+		yy.d = 0
+		if yy.walktimer< 7 then 
+		yy.im = bwalk1
+	elseif yy.walktimer>= 7 and yy.walktimer< 14 then
+		yy.im = bwalk2
+	elseif yy.walktimer>= 14 and yy.walktimer< 21 then
+		yy.im = bwalk3
+	elseif yy.walktimer>= 21 and yy.walktimer< 28 then
+		yy.im = bwalk4
+	elseif yy.walktimer>= 28 and yy.walktimer< 35 then
+		yy.im = bwalk5
 	else
-		walktimery= 0
+		yy.walktimer= 0
 	end
 
-
-
-	else yd = 0
-		you.bluechargetimer = 0
-		youbjstop = false
+	else yy.d = 0
+		yy.bluechargetimer = 0
+		yy.bjstop = false
 			
 	
 	end
 end
-
-	if younext > 0 and younext < 200
-	then youbjstop = true
+	if yy.next > 0 and yy.next < 200
+	then yy.bjstop = true
 end
-
-
 
 if ybp > -10000000000 
 		then
-		ybp = ybp - yd
+		ybp = ybp - yy.d
 	end
-
 	if ybp <= 0
-		then younext = younext + 1
+		then yy.next = yy.next + 1
 		ybp = 24
 	end
-
 	
 
-
-	if you.bluepause > 0
+	if yy.bluepause > 0
 		then
-	you.bluepause = you.bluepause - 1
-	you.stop = true
+	yy.bluepause = yy.bluepause - 1
+	yy.stop = true
 	end
-
-	if younext == 1 then you.stop = false
+	if yy.next == 1 then yy.stop = false
 	end
 end
 
-
-]]--
 
 
 
